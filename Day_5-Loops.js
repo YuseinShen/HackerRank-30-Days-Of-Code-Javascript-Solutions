6ddcfebc54639e248596668cfa0c4a8c32df0b39
YuseinShen <huseyinsen17@gmail.com>
2020-03-03 19:33:58 +0200

    Day 5 - Loops added

---
+++ Day_5-Loops.js
@@ -0,0 +1,10 @@
+function main() {
+    const n = parseInt(readLine(), 10);
+    const string = " x "
+    const string2= " = "
+    for (var i=1; i<=10; i++) {
+
+    console.log(n + string + i + string2 + (n*i) )
+        
+    }
+}
